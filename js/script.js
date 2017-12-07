594cce9784a6f37a3898a65ce9220bc4d49b3d8e
Fisher <truefisherking@gmail.com>
2017-12-07 12:00:45 -0500

    Added Functionality

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $("document").ready(function(){
 
-  $("li").click(function(){
+  $(".list").on("click", "li", function(){
     $(this).toggleClass("completed");
   });
 
-  $("span").click(function(){
+  $(".list").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+    });
+    event.stopPropagation();
+  });
 
+  $("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+      var todoText = $(this).val();
+      $(".list").append("<li><span>X </span>" + todoText + "</li>");
+      $(this).val("");
+    }
   });
 
 });
